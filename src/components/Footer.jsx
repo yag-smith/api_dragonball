5a56e3d934308f0a162ded4ad987ec228ffdcdde
yag-smith <yair.araujo@tecsup,edu.pe>
2025-12-03 14:21:42 -0500

    dasda

---
+++ src/components/Footer.jsx
@@ -0,0 +1,10 @@
+const Footer = () => {
+  return (
+    <footer className="bg-dark text-white text-center py-3 mt-auto">
+      <div className="container">
+        <p className="mb-0">React :/ </p>
+      </div>
+    </footer>
+  );
+};
+export default Footer;
